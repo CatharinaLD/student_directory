fa6b1a043fca6e8d78e3e9270e2f379c569f68d1
Catharina Luebke-Detring <catharina@Catharinas-MacBook-Pro.local>
2014-07-01 12:32:35 +0100

    Multiple students in user_input

--- day6/lib/directory.rb
+++ day6/lib/directory.rb
@@ -2,31 +2,40 @@ def print_header
 	puts 'Welcome to Student Directory'	
 end
 
-def print_students
-	puts 'Zoe, Anna, Catharina'
-end
-
 def students
 	#[{name:'Zoe'}, {name:'Anna'}, {name:'Catharina'}]
 	@students ||= []
-end
-
-def student_names
-	students.map{|student| student[:name]}
-end
-
-def add(student)
-	students << student
 end
 
 def ask_name 
 	puts 'Please enter your name'	
 end
 
-def get_name
+def add(student)
+	students << student
+end
+
+def user_input
 	gets.chomp
 end
 
-	def print_footer
+def student_names
+	students.map{|student| student[:name]}
+end
+
+def print_students
+	puts student_names.join(', ')
+end
+
+def print_footer
 	puts "Overall, we have #{students.length} students."
 end
+
+def create_student_named(name)
+	{name: name}
+end
+
+# print_header
+# ask_name
+# add(create_student_named(user_input))
+# print_footer
--- day6/spec/directory_spec.rb
+++ day6/spec/directory_spec.rb
@@ -10,34 +10,50 @@ describe "student directory" do
 		expect(students).to be_empty
 	end
 
-	it 'ask for a new student' do
-		expect(self).to receive(:puts).with('Please enter your name')
-		ask_name
+	context 'asking for input' do
+
+		it 'ask for a new student' do
+			expect(self).to receive(:puts).with('Please enter your name')
+			ask_name
+		end
+
+		it 'gets new student input' do
+			input = 'Zoe'
+			expect(self).to receive(:gets).and_return(input)
+			expect(user_input).to eq 'Zoe'
+		end
+
+		it 'gets new student input removing the new line' do
+			input = "Zoe\n"
+			expect(self).to receive(:gets).and_return(input)
+			expect(user_input).to eq "Zoe"
+		end
 	end
 
-	it 'gets new student input' do
-		input = 'Zoe'
-		expect(self).to receive(:gets).and_return(input)
-		expect(get_name).to eq 'Zoe'
-	end
+	context 'creating first student' do
 
-	it 'can add a student' do
-		student = {name: 'Zoe'}
-		add(student)
-		expect(students).to eq [student]
-	end
-
-	it 'prints footer' do
-		expect(self).to receive(:puts).with("Overall, we have #{students.length} students.")
-		print_footer
+		it 'creates a student with a name' do
+			student = {name: 'Chloe'}
+			expect(create_student_named('Chloe')).to eq student
+		end
+		
+		it 'can add a student' do
+			student = {name: 'Zoe'}
+			add(student)
+			expect(students).to eq [student]
+		end
 	end
 
 	context 'with three students' do
 
 		before do
 			%w{Zoe Anna Catharina}.each do |name|
-				add({name: name})
+				add( { name: name } )
 			end
+		end
+
+		it 'has three students' do
+			expect(students).to eq [{name: 'Zoe'},{name: 'Anna'},{name: 'Catharina'}]
 		end
 
 		it 'returns the names of the students' do
@@ -49,10 +65,17 @@ describe "student directory" do
 			print_students
 		end
 
-		it 'prints footer with right number of students' do
-			expect(self).to receive(:puts).with("Overall, we have #{students.length} students.")
-			print_footer
+		it 'prints the students when there are more' do
+			add(create_student_named('Enrique'))
+			expect(self).to receive(:puts).with(student_names.join(', '))
+			print_students
 		end
+
+	end
+	
+	it 'prints footer' do
+		expect(self).to receive(:puts).with("Overall, we have #{students.length} students.")
+		print_footer
 	end
 
 end
